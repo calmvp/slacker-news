f32315936e20ec4eddb09a72e73d92d386b7647c
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-31 12:23:59 -0500

    initiates project with rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HnClone::Application.config.secret_token = '8be1eaa9cbb601cb27bb33f1aae637965e16780151ea482e784ec1f4720ce7841f0c6bd7bf24b923c07f91495f298a402fde6bbc12ea85d5acb2b3cf0a409537'
