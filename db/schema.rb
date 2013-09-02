3b0776717428b11921e53573ae7b8346eae5171f
Ned KY <nkaidayip@gmail.com>
2013-09-01 21:23:51 -0500

    adds comment functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130901211402) do
+ActiveRecord::Schema.define(:version => 20130902020153) do
+
+  create_table "comments", :force => true do |t|
+    t.string  "body"
+    t.integer "post_id"
+    t.integer "user_id"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
